08bb6e7cb4730b69176c246c8555b5cc9659e38a
FurkanOzek <fur_ozek@hotmail.com>
2023-03-06 23:22:37 +0300

    sa

--- main.java
+++ main.java
@@ -5,4 +5,8 @@ public class main {
         System.out.println("Ahmet was here.");
         System.out.println("31 31");
     }
+
+    public void selamlar(){
+        System.out.println("yagizbrotv like atın");
+    }
 }
